66e5471b07e9ae6722391629f0926dd4edf10627
Ali <alkhdyyr@gmail.com>
2023-07-31 21:45:07 +0200

    just to practice local storage

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 let lis = document.querySelectorAll("ul li");
 let exp = document.querySelector(".experiment");
-
  if(window.localStorage.getItem("color")){
     exp.style.backgroundColor = window.localStorage.getItem("color");
     lis.forEach( (li) => {
         li.classList.remove('active');
        });
        document.querySelector(`[data-color=${ window.localStorage.getItem("color")}]`).classList.add('active')
-
   }else{
 console.log("the is no color");
   }
-
 lis.forEach((li) => {
     li.addEventListener("click", (e) => {
        // console.log(e.currentTarget.dataset.color);
